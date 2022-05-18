02248c0d1633268e8bea62d7fd3d22bac8e9984e
Lucas Andres Sanchez Rojas <lucas05091987@gmail.com>
2022-05-18 12:23:13 -0300

    probando function devolverString - JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -26,7 +26,7 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  return devolverString;
+  return str;
   // "Return" la string provista: str
   // Tu código:
   
